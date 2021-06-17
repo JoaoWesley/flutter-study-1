3bb741ee09a3d0d62f05723687fabad537091e15
João Wesley <joaowesley527@gmail.com>
2021-06-17 18:30:05 -0300

    finishing second module

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,88 @@
 import 'package:flutter/material.dart';
 
-void main() =>
-    runApp(MaterialApp(
-      home: Scaffold(
-        body: TransferList(),
-        appBar: AppBar(title: Text('Transferências'),),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
-      )
+void main() => runApp(ByteBankApp());
+
+class ByteBankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      body: TransferForm(),
     ));
+  }
+}
+
+class TransferForm extends StatelessWidget {
+  final TextEditingController _accountNumberFieldController = TextEditingController();
+  final TextEditingController _valueFieldController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Crinado Transferência')),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _accountNumberFieldController,
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                  labelText: 'Número da conta', hintText: '0000'),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _valueFieldController,
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: 'Valor',
+                  hintText: '0000'),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              debugPrint('clicou confirmar');
+              final double value = double.tryParse(_valueFieldController.text);
+              final int accountNumber = int.tryParse(_accountNumberFieldController.text);
+
+              if(accountNumber != null && value != null) {
+                final createdTransfer = Transfer(value, accountNumber);
+                debugPrint('$createdTransfer');
+              }
+            },
+            child: Text('CONFIRMAR'),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 class TransferList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TransferItem(Transfer(100.0, 100000)),
-        TransferItem(Transfer(200.0, 200000)),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      body: Column(
+        children: [
+          TransferItem(Transfer(100.0, 100000)),
+          TransferItem(Transfer(200.0, 200000)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
@@ -34,9 +98,7 @@ class TransferItem extends StatelessWidget {
         child: ListTile(
             leading: Icon(Icons.monetization_on),
             title: Text(this._transfer.value.toString()),
-            subtitle: Text(this._transfer.accountNumber.toString())
-        )
-    );
+            subtitle: Text(this._transfer.accountNumber.toString())));
   }
 }
 
@@ -45,4 +107,9 @@ class Transfer {
   final int accountNumber;
 
   Transfer(this.value, this.accountNumber);
+
+  @override
+  String toString() {
+    return 'Transfer{value: $value, accountNumber: $accountNumber}';
+  }
 }
