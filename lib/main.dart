ec1863f77e0dacf5a997caf59d0ae44ca05684f2
João Wesley <joaowesley527@gmail.com>
2021-06-18 18:05:48 -0300

    extracting flexible widgets

--- lib/main.dart
+++ lib/main.dart
@@ -13,55 +13,71 @@ class ByteBankApp extends StatelessWidget {
 }
 
 class TransferForm extends StatelessWidget {
-  final TextEditingController _accountNumberFieldController = TextEditingController();
+  final TextEditingController _accountNumberFieldController =
+      TextEditingController();
   final TextEditingController _valueFieldController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Crinado Transferência')),
+      appBar: AppBar(title: Text('Criando Transferência')),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
+          Editor(
               controller: _accountNumberFieldController,
-              style: TextStyle(
-                fontSize: 24.0,
-              ),
-              decoration: InputDecoration(
-                  labelText: 'Número da conta', hintText: '0000'),
-              keyboardType: TextInputType.number,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
+              tip: '0000',
+              label: 'Número da conta'),
+          Editor(
               controller: _valueFieldController,
-              style: TextStyle(
-                fontSize: 24.0,
-              ),
-              decoration: InputDecoration(
-                  icon: Icon(Icons.monetization_on),
-                  labelText: 'Valor',
-                  hintText: '0000'),
-              keyboardType: TextInputType.number,
-            ),
-          ),
+              tip: 'Valor',
+              label: '0.00',
+              icon: Icons.monetization_on),
           ElevatedButton(
             onPressed: () {
-              debugPrint('clicou confirmar');
-              final double value = double.tryParse(_valueFieldController.text);
-              final int accountNumber = int.tryParse(_accountNumberFieldController.text);
-
-              if(accountNumber != null && value != null) {
-                final createdTransfer = Transfer(value, accountNumber);
-                debugPrint('$createdTransfer');
-              }
+              createTransfer();
             },
             child: Text('CONFIRMAR'),
           ),
         ],
+      ),
+    );
+  }
+
+  void createTransfer() {
+    debugPrint('clicou confirmar');
+    final double value = double.tryParse(_valueFieldController.text);
+    final int accountNumber =
+        int.tryParse(_accountNumberFieldController.text);
+
+    if (accountNumber != null && value != null) {
+      final createdTransfer = Transfer(value, accountNumber);
+      debugPrint('$createdTransfer');
+    }
+  }
+}
+
+class Editor extends StatelessWidget {
+  final TextEditingController controller;
+  final String label;
+  final String tip;
+  final IconData icon;
+
+  Editor({this.controller, this.label, this.tip, this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: controller,
+        style: TextStyle(
+          fontSize: 24.0,
+        ),
+        decoration: InputDecoration(
+            icon: icon != null ? Icon(icon) : null,
+            labelText: label,
+            hintText: tip),
+        keyboardType: TextInputType.number,
       ),
     );
   }
